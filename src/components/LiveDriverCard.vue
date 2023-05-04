5c7b0511105e2f927fcfcbdde8b4728fe5b98a03
malik250danish <danish@250ahmad@gmail.com>
2023-05-04 18:32:47 +0500

    new

---
+++ src/components/LiveDriverCard.vue
@@ -0,0 +1,47 @@
+
+<template>
+    <!-- single item -->
+    <div class="col-md-6">
+        <div class="main-card padding-card">
+            <div class="row bg-row p-2">
+                <div class="col-lg-7 d-flex align-items-center">
+                    <img class="me-2" src="../assets/img/sam.png" alt="sam">
+                    <h5>Example heading <span class="badge">Added</span></h5>
+                </div>
+                <div class="col-lg-5">
+                    <ul class="list-group list-group-horizontal justify-content-end">
+                        <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
+                        <li class="list-group-item"><router-link to="/driverdetail"><img src="../assets/img/info.png"
+                                    alt="icon"></router-link></li>
+                        <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
+                        <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-7">
+                    <ul class="card-details">
+                        <li><b>Personal Information</b></li>
+                        <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
+                        <li>Address: <span> 744 564 564 56</span></li>
+                    </ul>
+                </div>
+                <div class="col-lg-5">
+                    <ul class="card-details">
+                        <li><b>Other Information</b></li>
+                        <li>Rental No.: <span>the demo</span></li>
+                        <li>Licence No.: <span>the demo</span></li>
+                        <li>State Issued: <span>the demo</span></li>
+                        <li>Unpaid Balance: <span>the demo</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script >
+export default {
+    name: 'LiveDriveCard',
+}
+</script>
--- src/views/LiveDriverView.vue
+++ src/views/LiveDriverView.vue
@@ -20,238 +20,20 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><a href="drivers-details.html"></a><i
-                                        class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LiveDriverCard />
 
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../assets/img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LiveDriverCard />
         </div>
 
         <div class="row mt-4">
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../assets/img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LiveDriverCard />
 
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../assets/img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LiveDriverCard />
         </div>
 
         <div class="row mt-4">
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../assets/img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-6 ">
-                <div class="main-card padding-card">
-                    <div class="row bg-row p-2">
-                        <div class="col-lg-7 d-flex align-items-center">
-                            <img class="me-2" src="../assets/img/sam.png" alt="sam">
-                            <h5>Example heading <span class="badge">Added</span></h5>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="list-group list-group-horizontal justify-content-end">
-                                <li class="list-group-item"><i class="fa-regular fa-eye grn"></i></li>
-                                <li class="list-group-item"><a href="drivers-details.html"><img src="../assets/img/info.png"
-                                            alt="icon"></a></li>
-                                <li class="list-group-item"><i class="fa-regular fa-pen-to-square blu"></i></li>
-                                <li class="list-group-item"><i class="fa-regular fa-trash-can red"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-lg-7">
-                            <ul class="card-details">
-                                <li><b>Personal Information</b></li>
-                                <li>Address: <span>29 Budapest Street, Rooty Hill NSW, Australia</span></li>
-                                <li>Address: <span> 744 564 564 56</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-5">
-                            <ul class="card-details">
-                                <li><b>Other Information</b></li>
-                                <li>Rental No.: <span>the demo</span></li>
-                                <li>Licence No.: <span>the demo</span></li>
-                                <li>State Issued: <span>the demo</span></li>
-                                <li>Unpaid Balance: <span>the demo</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LiveDriverCard />
+            <LiveDriverCard />
         </div>
     </div>
 </template>
@@ -267,12 +49,18 @@
 </style>
   
 <script>
+import LiveDriverCard from '../components/LiveDriverCard.vue';
 export default {
     data() {
         return {
             activeTab: 'Personal Details'
         };
     },
+    components: {
+        LiveDriverCard,
+    },
+
+
     methods: {
         openTab(cityName) {
             this.activeTab = cityName;
